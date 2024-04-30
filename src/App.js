037b3da9bb71b4bbca6af2a7d1d679727920c50c
demmluk <demmluk@gmail.com>
2024-04-30 11:27:33 +0200

    hw-37 done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import Header from "./components/Header";
-import LeftSidebar from "./components/LeftSidebar";
 import MainContainer from "./components/MainContainer";
 function App() {
   return (
     <div className='main-wrapper'>
-        <Header/>
-        <LeftSidebar/>
         <MainContainer/>
     </div>
   );
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
-class MainContainer  extends React.Component {
-    constructor(props) {
-        super(props);
+const options = ['😈', '😮', '😁', '🥺', '🤪', '💅'];
 
-    }
+function MainContainer() {
+    const [voteCount, setVoteCount] = useState(Array(options.length).fill(0));
 
-    render() {
-        return (
-            <>
-                <span>MainContainer</span>
-            </>
-        )
-    }
+    const voteForOption = (index) => {
+        const newVoteCount = [...voteCount];
+        newVoteCount[index]++;
+        setVoteCount(newVoteCount);
+    };
+
+    const getWinningIndex = () => {
+        let maxVotes = -1;
+        let winningIndex = -1;
+        for (let i = 0; i < voteCount.length; i++) {
+            if (voteCount[i] > maxVotes) {
+                maxVotes = voteCount[i];
+                winningIndex = i;
+            }
+        }
+        return winningIndex;
+    };
+
+    const showWinner = () => {
+        const winnerIndex = getWinningIndex();
+        const winnerEmoji = options[winnerIndex];
+        alert(`Winner emoji: ${winnerEmoji}`);
+    };
+
+    return (
+        <div>
+            <div id='voting-container'>
+                {options.map((option, index) => (
+                    <div className="smiley-container" key={index}>
+                        <div className="smiley" onClick={() => voteForOption(index)}>
+                            {option}
+                        </div>
+                        <div className="vote-count">
+                            {voteCount[index]}
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <button id='show-results' onClick={showWinner}>Show result</button>
+        </div>
+    );
 }
 
-export default MainContainer
+export default MainContainer;
